3acaf4f52c13460b3cbc679f9fbcdc887f6dfb5f
Games4Doritos <evanmiocevich@gmail.com>
2026-08-30 00:14:16 +0800

    Added good termination handling, made static lines better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from threading import Thread, Lock
+from threading import Thread, Lock, Event
 import random
+import os
 from time import sleep
 
 class State:
@@ -10,42 +11,54 @@ class State:
 globalState = State()
 globalLock = Lock()
 
-def _staticLine(string: str) -> str:
-    return f"\r{string}"
+exitSignal = Event()
 
-def _coinChange():
+def _coinChange(exitEvent: Event):
     global globalState
     while True:
+        if exitEvent.is_set():
+            break
         with globalLock:
             globalState.coins = int(globalState.coins * globalState.rate)
         sleep(0.2)
             
-def _rateChange():
+def _rateChange(exitEvent: Event):
     global globalState
     while True:
+        if exitEvent.is_set():
+            break
         with globalLock:
             globalState.rate = round(0.9 + random.random()*0.6, 7)
         sleep(0.2)
 
-def _input():
+def _input(exitEvent: Event):
     global globalState
     while True:
+        sleep(0.05)
         """inp = input(_staticLine("Action: "))
         print("\033[1A\033[K", end="") #weird ANSI for going back to last line so illusion of static line
         if inp == 'q':
             break"""
-        print(f"\r Coins - {globalState.coins} | Rate - {globalState.rate}", end="")
-        #if globalState.rate >= 1.5:
-           # print('end')
-            #break
+        if exitEvent.is_set():
+            break
+        print(f"Coins - {globalState.coins} |")
+        print(f"Rate - {globalState.rate}")
+        print("\033[2A", end="")
 
 def main():
-    input, coinChange, rateChange = Thread(target=_input), Thread(target=_coinChange), Thread(target=_rateChange)
+    input = Thread(target=_input, args=(exitSignal,))
+    coinChange = Thread(target=_coinChange, args=(exitSignal,))
+    rateChange = Thread(target=_rateChange, args=(exitSignal,))
     rateChange.start()
     coinChange.start()
     input.start()
-
-
+    try:
+        while True:
+            sleep(0.1)
+    except KeyboardInterrupt:
+        exitSignal.set()
+        print("\033[2A")
+        os.system('cls' if os.name == 'nt' else 'clear')
 
 if __name__ == "__main__":
     main()
